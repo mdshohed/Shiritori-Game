2ad3ec7047fddc0c7f03e011ae14c3e356c90805
mdshohed <mdshohed170@gmail.com>
2024-11-03 23:04:20 +0600

    readme file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-// import './App.css'
-import { useState } from 'react'
+
 import Shiritory from './pages/Shiritory'
-import { Button } from 'antd';
 
 function App() {
-  
 
   return (
     <div className='flex justify-center items-center min-h-screen'>
